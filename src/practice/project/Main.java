838f56713222a0166f85fe1fcef3984a8bde0c95
marunemo <chys3697@gmail.com>
2021-09-05 18:38:49 +0900

    Test IDE git management2

--- src/practice/project/Main.java
+++ src/practice/project/Main.java
@@ -1,7 +1,14 @@
 package practice.project;
 
+import practice.project.Test;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Test t = new Test("Hello World!");
+        Test t1 = new Test("Hello World!");
+        Test t2 = new Test("Hell World!");
+        t.print();
+        t1.print();
+        t2.print();
     }
 }
